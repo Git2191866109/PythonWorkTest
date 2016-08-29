73158fdaac5db3c1f378eb0a2dab780bd3f9f447
wei.ma <2191866109@qq.com>
2016-08-29 14:48:26 +0800

    基础操作

--- com.mw.python/test/TestTime.py
+++ com.mw.python/test/TestTime.py
@@ -1,9 +1,11 @@
 # -*- coding:utf-8 -*-
-import time
-#print  time.time()
-#print time.localtime()
+import time, os
+
+# print  time.time()
+# print time.localtime()
 
 print int(time.time() + 3600 * 8 + 86400 * 3)
+
 
 def getfixeddate(fixhours, fixdays=0):
     print time.strftime("%Y-%m-%d", time.localtime(getfixedTimeStamp(fixhours, fixdays)))
@@ -19,6 +21,7 @@ def getfixedTimeStamp(fixhours, fixdays=0):
     print int(time.time() + 3600 * fixhours + 86400 * fixdays)
     return int(time.time() + 3600 * fixhours + 86400 * fixdays)
 
+
 # 比如今天是2016-08-26，如果j= 3 取的是26号的后三天即：2016-08-29，如果j=-3，取得是26号的前三天2016-08-23
 j = -7
 print j
@@ -26,3 +29,9 @@ print j
 date0 = getfixeddate(8, j)
 
 print date0
+# portraitday='2016-06-24'
+# monthday='2016-06-19'
+portraitday = os.popen("date +%Y-%m-%d '-d -2 day +8 hour'").read().strip()
+monthday = os.popen("date +%Y-%m-%d '-d -9 day +8 hour'").read().strip()
+print portraitday
+print monthday
--- com.mw.python/work/MysqlTest.py
+++ com.mw.python/work/MysqlTest.py
@@ -1,14 +1,15 @@
+import traceback
+
 import MySQLdb
-try:
-    conn = MySQLdb.connect(host='ec2-54-223-197-216.cn-north-1.compute.amazonaws.com.cn', user='moji',
-                           passwd='BigDataMoji', db='tblu', port=3306, charset="utf8")
-    cursor = conn.cursor()
-    content = [(1, 12, 12)]
-    sql = """insert into feed_access_click(click_num,date) values(%s,%s)"""
-    cursor.executemany(sql, content)
-    conn.commit()
-except:
-    print "Mysql Error %d: %s"
-finally:
-    cursor.close()
-    conn.close()
+
+def mysqlExe(sql):
+    try:
+        conn = MySQLdb.connect(host='ec2-54-223-197-216.cn-north-1.compute.amazonaws.com.cn', user='moji',passwd='BigDataMoji', db='userinfo', port=3306, charset="utf8")
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        conn.commit()
+    except:
+        traceback.print_exc()
+    finally:
+        cursor.close()
+        conn.close()
